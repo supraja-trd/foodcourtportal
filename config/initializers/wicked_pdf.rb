e2822669c705f10b061d256b5a832ba3055c5f64
supraja-trd <supraja.dinakaran@freshworks.com>
2021-12-22 11:32:21 +0530

    Revert "Food Court Portal - First Commit"
    This reverts commit 996cc2b48c68f15cbdc052b179253268e5f188b4.

--- config/initializers/wicked_pdf.rb
+++
@@ -1,5 +0,0 @@
-WickedPdf.config = {
-  #:wkhtmltopdf => '/usr/local/bin/wkhtmltopdf',
-  #:layout => "pdf.html",
-  # :exe_path => '/Users/sdinakaran/.rvm/gems/ruby-2.4.7@itildesk/bin/wkhtmltopdf'
-}
